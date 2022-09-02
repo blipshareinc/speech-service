790ce2615c913eb3e27464a6baec1e80129de6b5
BLIPShare <85604427+blipshare@users.noreply.github.com>
2022-09-01 23:00:51 -0400

    Added fastspeech2 processing and Dockerfile.

--- speech_service/README.md
+++ speech_service/README.md
@@ -1,5 +1,5 @@
 **<span style="color:yellow">WORK-IN-PROGRESS</span>**
-# Home Automation Voice Module
+# Speech Service
 This is the voice addon for the home automation project. It consists of two microservices hosted in containers.
 
 ## 1. Speech-To-Text Service
@@ -10,8 +10,7 @@ This service takes a `.wav` audio file as input and returns the corresponding te
 * Create a `virtual` env and install the following packages
   <ul>
     <li>Gunicorn</li>
-    <li>deepspeech</li>
-    <li>tensorflow</li>
+    <li>Flask</li>
   </ul>
 
 * Create a python web app to listen for requests with `.wav` file as an input and provide the text as output to the client
@@ -21,20 +20,7 @@ This service takes a `.wav` audio file as input and returns the corresponding te
 * Create a `Dockerfile`
 
 ## 2. Text-To-Speech Service
-This Service takes text as input and converts and returns a `.wav` audio file. It utilizes fastspeech2 model to do so.
-
-### To-Dos:
-* Create a `virtual` env and install the following packages
-  <ul>
-    <li>Gunicorn</li>
-    <li>fastspeech2</li>
-  </ul>
-
-* Create a python web app to listen for request with `text` as an input and return a `.wav` as an output to the client
-
-* Create a `requirements.txt` file
-
-* Create a `Dockerfile`
+This Service takes text as input and converts and returns a `.wav` audio file. It utilizes fastspeech2 model to do so. [Read more](./text_to_speech/README.md)
 
 ## Productionize
 The following steps should be done once both services can run as services in a container
---
+++ speech_service/text_to_speech/fastspeech2.py
@@ -0,0 +1,65 @@
+from tensorflow_tts.inference import TFAutoModel
+from tensorflow_tts.inference import AutoProcessor
+from datetime import datetime
+from os import path, makedirs
+
+import tensorflow as tf
+import numpy as np
+import soundfile as sf
+import yaml
+import argparse
+
+def process(text: str, output_dir: str):
+    '''
+    Description: Function to process input text and convert to audio (.wav) using FastSpeech2
+    @param: text -- str. Text to be converted.
+    @param: output_dir -- str. Absolute path of the output directory where audio file to be generated
+    @return: output_file -- str. Absolute path to the output audio (.wav) file
+    '''
+
+    # if the output directory does not exist, create one
+    if not path.exists(output_dir):
+        makedirs(output_dir)
+
+    # initialize fastspeech2 model.
+    fastspeech2 = TFAutoModel.from_pretrained("tensorspeech/tts-fastspeech2-ljspeech-en")
+
+    # initialize mb_melgan model
+    mb_melgan = TFAutoModel.from_pretrained("tensorspeech/tts-mb_melgan-ljspeech-en")
+
+    # inference
+    processor = AutoProcessor.from_pretrained("tensorspeech/tts-fastspeech2-ljspeech-en")
+
+    input_ids = processor.text_to_sequence(text)
+
+    # fastspeech inference
+
+    _, mel_after, _, _, _ = fastspeech2.inference(
+        input_ids=tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+        speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
+        speed_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
+        f0_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
+        energy_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
+    )
+
+    # melgan inference
+    audio_after = mb_melgan.inference(mel_after)[0, :, 0]
+
+    # save to file
+    datetime_str = datetime.today().strftime('%Y-%m-%d_%H_%M_%S')
+    output_file = path.join(output_dir, 'audio_%s.wav' % datetime_str)
+    sf.write(output_file, audio_after, 22050, "PCM_16")
+
+    # return path of the audio file
+    return output_file
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Text to speech")
+    parser.add_argument("-t", "--TEXT", dest="text", help="Text to be translated to audio", required=True)
+    parser.add_argument("-o", "--OUTPUT_DIR", dest="output_dir", help="Absolute path to output directory", required=True)
+
+    # parse arguments
+    args = parser.parse_args()
+
+    # process
+    process(args.text, args.output_dir)
